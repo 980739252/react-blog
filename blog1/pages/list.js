ab3e9085603384a6d8482fb26621704fd29215ad
980739252 <980739252@qq.com>
2020-09-11 01:04:50 +0800

    9-11

--- blog1/pages/list.js
+++ blog1/pages/list.js
@@ -1,8 +1,8 @@
 import React from 'react'
 import '../public/components/header.css'
-import {HomeTwoTone,PlaySquareTwoTone, SmileTwoTone  } from "@ant-design/icons"
+
 import {Row,Col, Menu} from 'antd'
-const Header = () => (
+const List = () => (
   <div className="header">
     <Row type="flex" justify="center">
         <Col  xs={24} sm={24} md={10} lg={15} xl={12}>
@@ -13,15 +13,15 @@ const Header = () => (
         <Col className="memu-div" xs={0} sm={0} md={14} lg={8} xl={6}>
             <Menu  mode="horizontal">
                 <Menu.Item key="home">
-                <HomeTwoTone />
+                    
                     首页
                 </Menu.Item>
                 <Menu.Item key="video">
-                <PlaySquareTwoTone />
+                
                     视频
                 </Menu.Item>
                 <Menu.Item key="life">
-                   <SmileTwoTone/>
+                   
                     生活
                 </Menu.Item>
             </Menu>
@@ -30,4 +30,4 @@ const Header = () => (
  </div>
 )
 
-export default Header
+export default List
