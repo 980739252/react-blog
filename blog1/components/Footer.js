aa383c9d32c950772060c9bf78a45599edcc30d1
980739252 <980739252@qq.com>
2020-09-15 19:57:23 +0800

    9-15

---
+++ blog1/components/Footer.js
@@ -0,0 +1,9 @@
+import '../public/components/footer.css'
+const Footer =()=>(
+    <div className="footer-div">
+    <div>系统由 React+Node+Ant Desgin驱动 </div>
+    <div>夜深人静街道的风</div>
+    </div>
+)
+
+export default Footer
--- blog1/pages/index.js
+++ blog1/pages/index.js
@@ -2,9 +2,10 @@ import React,{useState} from 'react'
 import Head from 'next/head'
 import Header from '../components/Header.js'
 import {CalendarFilled,FolderFilled,FireFilled} from "@ant-design/icons"
-import {Row,Col,List} from 'antd'
-import '../public/pages/index.css'
+import {Row,Col,List,Breadcrumb} from 'antd'
 import Author from '../components/Author'
+import Advert from '../components/Advert'
+import Footer from '../components/Footer'
  function Home() {
   const [ mylist , setMylist ] = useState(
     [
@@ -17,11 +18,18 @@ import Author from '../components/Author'
   return (
     <>
       <Head>
-        <title>Home</title>
+        <title>首页</title>
       </Head>
        <Header/>
        <Row className="comm-main" type="flex" justify="center">
           <Col className="comm-left" xs={24} sm={24} md={16} lg={18} xl={14}>
+          <div className="bread-div">
+           <Breadcrumb>
+            <Breadcrumb.Item><a href="/">首页</a></Breadcrumb.Item>
+            <Breadcrumb.Item>视频列表</Breadcrumb.Item>
+            <Breadcrumb.Item>生活碎片</Breadcrumb.Item>
+           </Breadcrumb>
+           </div>
             <List
               header={<div>最新日志</div>}
               itemLayout="vertical"
@@ -39,8 +47,12 @@ import Author from '../components/Author'
               )}
             />
           </Col>
-          <Col className="comm-right" xs={0} sm={0} md={7} lg={5} xl={4}><Author/></Col>
+          <Col className="comm-right" xs={0} sm={0} md={7} lg={5} xl={4}>
+            <Author/>
+            <Advert/>
+          </Col>
        </Row>
+       <Footer/>
     </>
    
   )
