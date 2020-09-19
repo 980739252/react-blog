118c53c4ff40fc02a187f2329ba8a744e6b2b235
980739252 <980739252@qq.com>
2020-09-19 23:52:18 +0800

    9-19

--- blog1/pages/detailed.js
+++ blog1/pages/detailed.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import Head from 'next/head'
+import axios from 'axios'
 import {Row, Col ,Breadcrumb,Affix  } from 'antd'
 import {CalendarFilled,FolderFilled,FireFilled} from "@ant-design/icons"
 import Header from '../components/Header'
@@ -109,6 +110,21 @@ let markdown=
    </>
     )
 }
-   
+Detailed.getInitialProps = async(context)=>{
+
+  console.log(context.query.id)
+  let id =context.query.id
+  const promise = new Promise((resolve)=>{
+
+    axios('http://127.0.0.1:7001/default/getArticleById/'+id).then(
+      (res)=>{
+        console.log(title)
+        resolve(res.data.data[0])
+      }
+    )
+  })
+
+  return await promise
+}   
 
 export default Detailed
--- service/app/controller/default/home.js
+++ service/app/controller/default/home.js
@@ -4,11 +4,43 @@ const Controller = require('egg').Controller;
 
 class HomeController extends Controller {
   async index() {
-    
-    let result = await this.app.mysql.get("blog_content",{})
-    console.log(result)
-    this.ctx.body=result
+     this.ctx.body="api hi"
   }
+  async getArticleList(){
+
+    let sql = 'SELECT article.id as id,'+
+              'article.title as title,'+
+              'article.introduce as introduce,'+
+              'article.view_count as view_count ,'+
+              '.type.typeName as typeName '+
+              'FROM article LEFT JOIN type ON article.type_id = type.Id'
+ 
+     const results = await this.app.mysql.query(sql)
+ 
+     this.ctx.body= {data:results}
+     
+ }
+ async getArticleById(){
+  let id = this.ctx.params.id
+
+  let sql = 'SELECT article.id as id,'+
+  'article.title as title,'+
+  'article.introduce as introduce,'+
+  'article.article_content as article_content,'+
+  'article.view_count as view_count ,'+
+  'type.typeName as typeName ,'+
+  'type.id as typeId '+
+  'FROM article LEFT JOIN type ON article.type_id = type.Id '+
+  'WHERE article.id='+id
+
+
+
+  const result = await this.app.mysql.query(sql)
+
+
+  this.ctx.body={data:result}
+    
+ }
 }
 
 module.exports = HomeController;
